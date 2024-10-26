483559efb5e26a16c6de31ef1f7199dff570b3cb
Nutsandbolts800 <alan.copeland@protonmail.com>
2024-10-26 14:39:44 -0700

    Add excpetion handling for POST request

--- pager.py
+++ pager.py
@@ -6,15 +6,21 @@ class pager:
         self.number = number
     
     def send_page(self, message):
-        data = 'PIN={0}&MSSG={1}'.format(self.number, message)
-        r = requests.post('http://www.usamobility.net/cgi-bin/wwwpage.exe', data=data)
-        return r
+        if len(message) < 240:
+            data = 'PIN={0}&MSSG={1}'.format(self.number, message)
+            try:
+                r = requests.post('http://www.usamobility.net/cgi-bin/wwwpage.exe', data=data)
+            except Exception as e:
+                raise e
+            return r
+        else:
+            print('Error: Message too long. More than 240 characters')
+            return 1
 
 if __name__ == "__main__":
     try:
-        if sys.argv[1]:
-            p = pager(sys.argv[1])
-            if sys.argv[2]:
-                p.send_page(sys.argv[2])
+        p = pager(sys.argv[1])
+        p.send_page(sys.argv[2])
+
     except:
         print("usage: page.py [number] [message] or import and use the pager class")
